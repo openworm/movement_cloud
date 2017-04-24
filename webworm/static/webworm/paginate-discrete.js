08a61d74864eef273824fff7053dfb9697a74a9f
Chee Wai Lee <cheewai1972@gmail.com>
2017-04-24 13:47:00 +0800

    Implemented select/deselect support for all discrete fields in a flexible parameterized manner that allows for fields to be added and dropped. This currently only applies to the Javascript code, and the same will probably need to be implemented for dynamically modifying the HTML code using jQuery at some point.

--- webworm/static/webworm/paginate-discrete.js
+++ webworm/static/webworm/paginate-discrete.js
@@ -1,158 +1,104 @@
 // *CWL* - A lot of hardcodes to get an early prototype working. 
 //        Can be refactored to be better parameterized. Please fix.
-var selectedStrainsIndices = {};
-var selectedTrackersIndices = {};
-var selectedSexIndices = {};
-var selectedDevIndices = {};
-var selectedVentralIndices = {};
-var selectedFoodIndices = {};
-var selectedArenaIndices = {};
-var selectedHabituationIndices = {};
-var selectedExperimentersIndices = {};
+var discreteMap = [
+		   'strains',
+		   'trackers',
+		   'sex',
+		   'dev',
+		   'ventral',
+		   'food',
+		   'arena',
+		   'habituation',
+		   'experimenter',
+		   ];
 var hiddenDiscreteIndex = 1;
 
 $(document).ready(function() {
 
-	var strainsTable = $('#sum_strains').DataTable( {
-		dom: 'Blfrtip',
-		buttons: [
-			  'selectAll',
-			  'selectNone',
-			  ],
-		select: {
-		    style: 'multi'
-		}
-	    });
-
-	var trackersTable = $('#sum_trackers').DataTable( {
-		dom: 'Blfrtip',
-		buttons: [
-			  'selectAll',
-			  'selectNone',
-			  ],
-		select: {
-		    style: 'multi'
-		}
-	    });
-
-	var sexTable = $('#sum_sex').DataTable( {
-		dom: 'Blfrtip',
-		buttons: [
-			  'selectAll',
-			  'selectNone',
-			  ],
-		select: {
-		    style: 'multi'
-		}
-	    });
-
-	var devTable = $('#sum_dev').DataTable( {
-		dom: 'Blfrtip',
-		buttons: [
-			  'selectAll',
-			  'selectNone',
-			  ],
-		select: {
-		    style: 'multi'
-		}
-	    });
-
-	var ventralTable = $('#sum_ventral').DataTable( {
-		dom: 'Blfrtip',
-		buttons: [
-			  'selectAll',
-			  'selectNone',
-			  ],
-		select: {
-		    style: 'multi'
-		}
-	    });
-
-	var foodTable = $('#sum_food').DataTable( {
-		dom: 'Blfrtip',
-		buttons: [
-			  'selectAll',
-			  'selectNone',
-			  ],
-		select: {
-		    style: 'multi'
-		}
-	    });
-
-	var arenaTable = $('#sum_arena').DataTable( {
-		dom: 'Blfrtip',
-		buttons: [
-			  'selectAll',
-			  'selectNone',
-			  ],
-		select: {
-		    style: 'multi'
-		}
-	    });
-
-	var habitTable = $('#sum_habit').DataTable( {
-		dom: 'Blfrtip',
-		buttons: [
-			  'selectAll',
-			  'selectNone',
-			  ],
-		select: {
-		    style: 'multi'
-		}
-	    });
-	var exprTable = $('#sum_experimenter').DataTable( {
-		dom: 'Blfrtip',
-		buttons: [
-			  'selectAll',
-			  'selectNone',
-			  ],
-		select: {
-		    style: 'multi'
-		}
-	    });
-
-	var confirmStrainsTable = $('#confirmStrains').DataTable( {
-		"columnDefs": [
-	{
-	    "targets":[hiddenDiscreteIndex],
-	    "visible": false,
-	    "searchable": false
-	},
-			       ],
-	});
-
-	strainsTable.on( 'select', function ( e, dt, type, indexes ) {
+	var selectEventFactory = function(discreteTable, confirmTable, discreteIndex) {
+	    var returnFunction = function(e, dt, type, indexes) {
+		var localDiscreteTable = discreteTable;
+		var localConfirmTable = confirmTable;
+		var localDiscreteIndex = discreteIndex;
 		if ( type === 'row' ) {
-		    var origData = strainsTable.rows(indexes).data();
+		    var origData = localDiscreteTable.rows(indexes).data();
 		    var selectLength = indexes.length;
-		    for (var i=0; i<selectLength; i++) {
-			if (!selectedStrainsIndices[indexes[i]]) {
-			    selectedStrainsIndices[indexes[i]] = "true";
+		    for (var sIdx=0; sIdx<selectLength; sIdx++) {
+			if (!localDiscreteIndex[indexes[sIdx]]) {
+			    localDiscreteIndex[indexes[sIdx]] = "true";
 			    var data = [];
 			    // *CWL* This is an annoying hardcode.
-			    data.push(origData[i][0]); // name of parameter
-			    data.push(indexes[i]);
-			    confirmStrainsTable.row.add(data);
+			    data.push(origData[sIdx][0]); // name of parameter
+			    data.push(indexes[sIdx]);
+			    localConfirmTable.row.add(data);
 			}
 		    }
-		    confirmStrainsTable.draw();
+		    localConfirmTable.draw();
 		}
-	    } );
-
-	strainsTable.on( 'deselect', function ( e, dt, type, indexes ) {
+	    }
+	    return returnFunction;
+	}
+	
+	var deselectEventFactory = function(discreteTable, confirmTable, discreteIndex) {
+	    var returnFunction = function(e, dt, type, indexes) {
+		var localDiscreteTable = discreteTable;
+		var localConfirmTable = confirmTable;
+		var localDiscreteIndex = discreteIndex;
 		if ( type === 'row' ) {
 		    var selectLength = indexes.length;
-		    for (var i=0; i<selectLength; i++) {
-			if (selectedStrainsIndices[indexes[i]]) {
-			    delete selectedStrainsIndices[indexes[i]];
-			    confirmStrainsTable.row( function(idx, data, node){
-				    return data[hiddenDiscreteIndex] === indexes[i]?true:false;
+		    for (var sIdx=0; sIdx<selectLength; sIdx++) {
+			if (localDiscreteIndex[indexes[sIdx]]) {
+			    delete localDiscreteIndex[indexes[sIdx]];
+			    localConfirmTable.row( function(idx, data, node){
+				    return data[hiddenDiscreteIndex] === 
+					indexes[sIdx]?true:false;
 				}).remove();
 			}
 		    }
-		    confirmStrainsTable.draw();
+		    localConfirmTable.draw();
 		}
-	    } );
+	    }
+	    return returnFunction;
+	}
 
+	var discreteIndices = new Array(discreteMap.length);
+	for (var disIdx=0; disIdx<discreteMap.length; disIdx++) {
+	    discreteIndices[disIdx] = {};
+	}
+
+	var discreteTables = new Array(discreteMap.length);
+	var confirmTables = new Array(discreteMap.length);
+
+	for (var disIdx=0; disIdx<confirmTables.length; disIdx++) {
+	    confirmTables[disIdx] = $('#'+discreteMap[disIdx]+'Confirm').DataTable( {
+		    "columnDefs": [ {
+			    "targets":[hiddenDiscreteIndex],
+			    "visible": false,
+			    "searchable": false
+			},],
+		    "bFilter": false,
+		    deferRender:    true,
+		    scrollY:        200,
+		    scrollCollapse: true,
+		    scroller:       true,
+		});
+	    discreteTables[disIdx] = $('#sum_'+ discreteMap[disIdx]).DataTable( {
+		    dom: 'Blfrtip',
+		    buttons: [
+			      'selectAll',
+			      'selectNone',
+			      ],
+		    select: {
+			style: 'multi'
+		    }
+		});
+	    var discreteTable = discreteTables[disIdx];
+	    var discreteIndex = discreteIndices[disIdx];
+	    var confirmTable = confirmTables[disIdx];
+	    discreteTable.on('select', 
+			     selectEventFactory(discreteTable, confirmTable, discreteIndex));
+	    discreteTable.on('deselect', 
+			     deselectEventFactory(discreteTable, confirmTable, discreteIndex));
+	}
 
     } );
