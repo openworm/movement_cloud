8b05e28821c53524083c61475a65d4fd7e78a8b0
MichaelCurrie <mcurrie@bruceforceresearch.com>
2017-05-13 18:45:02 +0700

    Remove unused user parameters

--- crossfilter_example/crossfilter_parameters.js
+++ crossfilter_example/crossfilter_parameters.js
@@ -1,9 +1,4 @@
 var XFILTER_PARAMS = {
-
-    "user_fields": [
-        {"field": "delay", "display_name": "Time of Day", "format": ""},
-        {"field": "distance", "display_name": "Arrival Delay (min.)", "format": ""}
-    ],
     "display_fields": [
         {"data_field": "origin", "data_type": "string", "display_name": "Origin"},
         {"data_field": "destination", "data_type": "string", "display_name": "Destination"},
@@ -14,23 +9,5 @@ var XFILTER_PARAMS = {
     "data_file": "worm_mock_data.csv",
     "report_title": "Worm Crossfilter Demonstration",
 
-    "format1": "",
-    "format2": "",
-
-
-    "footer_text": "A demonstration of animated bubble charts in JavaScript and D3.js",
-    "width": 940,
-    "height": 700,
-    "force_strength": 0.03,
-    "force_type": "charge",
-    "radius_field": "Population",
-    "numeric_fields": ["Area", "Population", "Density"],
-    "fill_color": {
-        "data_field": "Density Level",
-        "color_groups": {
-            "low": "#d84b2a",
-            "medium": "#beccae",
-            "high": "#7aa25c"
-        }
-    }
+    "max_results": 15,
 }
