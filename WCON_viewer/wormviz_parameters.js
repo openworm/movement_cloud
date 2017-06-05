f7ac2625b378ece3cf69c4484981e47f6fee6a57
MichaelCurrie <mcurrie@bruceforceresearch.com>
2017-06-04 20:52:46 -0600

    Move worm viz to top

--- WCON_viewer/wormviz_parameters.js
+++ WCON_viewer/wormviz_parameters.js
@@ -4,7 +4,8 @@ var WORMVIZ_PARAMS = {
     "schema_url": "https://raw.githubusercontent.com/openworm/tracker-commons/master/wcon_schema.json",
     
     "worm_petri_dish": {
-        "radius": 200,
+        "max_height": 400,
+        "margin" = {"left": 50, "top": 25, "right": 25, "bottom": 25}
         "NUM_WORMS": 5,
         "m": 12
     }
