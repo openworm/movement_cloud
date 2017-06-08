f139cf30340b0b076329551e8b559b9a1831d3fd
Michael Currie <mcurrie@bruceforceresearch.com>
2017-06-07 20:21:42 -0600

    Dropzone demo: Simplify HTTP server further

--- WCON_viewer/upload_handler2.py
+++ WCON_viewer/upload_handler2.py
@@ -7,25 +7,6 @@ import sys
 from http.server import HTTPServer # For the Server
 from http.server import SimpleHTTPRequestHandler  # For the Handler
 
-"""
-HandlerClass = SimpleHTTPRequestHandler
-
-# Specify the bind address and port
-server_address = ("", 8000)
-
-HandlerClass.protocol_version = "HTTP/1.0"
-with HTTPServer(server_address, HandlerClass) as httpd:
-    sa = httpd.socket.getsockname()
-    serve_message = ("Now serving HTTP on {host} port {port} " +
-                    "(http://{host}:{port}/) ...")
-    print(serve_message.format(host=sa[0], port=sa[1]))
-    try:
-        httpd.serve_forever()
-    except KeyboardInterrupt:
-        print("\nKeyboard interrupt received, exiting.")
-        sys.exit(0)
-"""
-
 SimpleHTTPRequestHandler.protocol_version = "HTTP/1.0"
 httpd = HTTPServer(("", 8000), SimpleHTTPRequestHandler)
 
