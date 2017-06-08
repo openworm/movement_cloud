9682ab10b251c4ac0f4a9f26e89adbcf15630ba8
Michael Currie <mcurrie@bruceforceresearch.com>
2017-06-07 20:21:06 -0600

    Simple HTTP server for drag and drop demo

--- WCON_viewer/upload_handler2.py
+++ WCON_viewer/upload_handler2.py
@@ -3,25 +3,37 @@
 Handle WCON uploads from our site
 """
 
-from http.server import SimpleHTTPRequestHandler, CGIHTTPRequestHandler, test
-import argparse
+import sys
+from http.server import HTTPServer # For the Server
+from http.server import SimpleHTTPRequestHandler  # For the Handler
 
-class MichaelHTTPRequestHandler(SimpleHTTPRequestHandler):
-    pass
+"""
+HandlerClass = SimpleHTTPRequestHandler
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--cgi', action='store_true',
-                   help='Run as CGI Server')
-parser.add_argument('--bind', '-b', default='', metavar='ADDRESS',
-                    help='Specify alternate bind address '
-                         '[default: all interfaces]')
-parser.add_argument('port', action='store',
-                    default=8000, type=int,
-                    nargs='?',
-                    help='Specify alternate port [default: 8000]')
-args = parser.parse_args()
-if args.cgi:
-    handler_class = CGIHTTPRequestHandler
-else:
-    handler_class = SimpleHTTPRequestHandler
-test(HandlerClass=handler_class, port=args.port, bind=args.bind)
+# Specify the bind address and port
+server_address = ("", 8000)
+
+HandlerClass.protocol_version = "HTTP/1.0"
+with HTTPServer(server_address, HandlerClass) as httpd:
+    sa = httpd.socket.getsockname()
+    serve_message = ("Now serving HTTP on {host} port {port} " +
+                    "(http://{host}:{port}/) ...")
+    print(serve_message.format(host=sa[0], port=sa[1]))
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        print("\nKeyboard interrupt received, exiting.")
+        sys.exit(0)
+"""
+
+SimpleHTTPRequestHandler.protocol_version = "HTTP/1.0"
+httpd = HTTPServer(("", 8000), SimpleHTTPRequestHandler)
+
+sa = httpd.socket.getsockname()
+print("Serving HTTP on", sa[0], "port", sa[1], "...")
+try:
+    httpd.serve_forever()
+except KeyboardInterrupt:
+    print("\nKeyboard interrupt received, exiting.")
+    httpd.server_close()
+    sys.exit(0)
